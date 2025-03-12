5188bac9e8bb6098d5d84fdf62bd383761b66025
alivemaster <alivemaster3@gmail.com>
2025-03-12 20:24:59 +0800

    refactor: i forgot something in ea94797

--- src/handlers/score.rs
+++ src/handlers/score.rs
@@ -6,8 +6,8 @@ pub async fn handler(
     Path(paths): Path<ScorePaths>,
 ) -> Result<ScoreResponse, OsuErrorResponse> {
     let score = osu_client.score(paths.score_id);
-    let score = if let Some(game_mode) = paths.game_mode {
-        score.mode(GameMode::from(game_mode))
+    let score = if let Some(mode) = paths.mode {
+        score.mode(GameMode::from(mode))
     } else {
         score
     };
@@ -23,14 +23,14 @@ pub async fn handler(
 
 #[derive(Deserialize)]
 pub struct ScorePaths {
-    pub game_mode: Option<u8>,
+    pub mode: Option<u8>,
     pub score_id: u64,
 }
 
 impl Clone for ScorePaths {
     fn clone(&self) -> Self {
         Self {
-            game_mode: self.game_mode,
+            mode: self.mode,
             score_id: self.score_id,
         }
     }
