19921f9c82962b99516e9d2b30c210ebb80b4dc1
alivemaster <alivemaster3@gmail.com>
2026-05-06 17:23:02 +0800

    refactor: simplify beatmap mode and mods params handling

--- src/utils/cal_beatmap_attrs.rs
+++ src/utils/cal_beatmap_attrs.rs
@@ -1,5 +1,8 @@
-use rosu_pp::model::beatmap::BeatmapAttributesBuilder;
-use rosu_v2::prelude::{BeatmapExtended, GameModIntermode, GameModsIntermode};
+use rosu_pp::model::{
+    beatmap::BeatmapAttributesBuilder,
+    mods::rosu_mods::{GameMode, GameMods, GameModsIntermode},
+};
+use rosu_v2::prelude::BeatmapExtended;
 
 pub fn cal_beatmap_attrs(
     beatmap: &mut BeatmapExtended,
@@ -13,39 +16,34 @@ pub fn cal_beatmap_attrs(
         .od(beatmap.od, false)
         .hp(beatmap.hp, false);
 
-    // is a converted map?
+    // game mode specified?
     if let Some(mode) = mode {
-        if beatmap.mode as u8 != mode {
-            if beatmap.mode as u8 == 0 {
-                // can convert
-                attrs_builder.mode(mode.into(), true);
-
-                // is a converted map
-                beatmap.convert = true;
-                beatmap.mode = mode.into();
-            } else {
-                // can't convert
+        let game_mode = GameMode::from(mode);
+        if beatmap.mode != game_mode {
+            // can't convert
+            if beatmap.mode != GameMode::Osu {
                 return;
             }
+
+            // is a converted map
+            beatmap.mode = game_mode;
+            beatmap.convert = true;
         }
-    } else {
-        attrs_builder.mode((beatmap.mode as u8).into(), false);
-    };
+    }
+
+    attrs_builder.mode((beatmap.mode as u8).into(), beatmap.convert);
 
     // has mods?
     if let Some(mods) = &mods {
         attrs_builder.mods(mods);
 
-        if mods.contains(GameModIntermode::DoubleTime) || mods.contains(GameModIntermode::Nightcore)
+        // DT/NC or HT/DC
+        if let Some(clock_rate) = GameMods::from_intermode(mods, beatmap.mode).clock_rate()
+            && clock_rate != 1.0
         {
-            beatmap.bpm = beatmap.bpm * 1.5;
-            beatmap.seconds_drain = (beatmap.seconds_drain as f64 / 1.5) as u32;
-            beatmap.seconds_total = (beatmap.seconds_total as f64 / 1.5) as u32;
-        }
-        if mods.contains(GameModIntermode::HalfTime) || mods.contains(GameModIntermode::Daycore) {
-            beatmap.bpm = beatmap.bpm * 0.75;
-            beatmap.seconds_drain = (beatmap.seconds_drain as f64 / 0.75) as u32;
-            beatmap.seconds_total = (beatmap.seconds_total as f64 / 0.75) as u32;
+            beatmap.bpm = (beatmap.bpm as f64 * clock_rate) as f32;
+            beatmap.seconds_drain = (beatmap.seconds_drain as f64 / clock_rate) as u32;
+            beatmap.seconds_total = (beatmap.seconds_total as f64 / clock_rate) as u32;
         }
     }
 
